b88615e996aac6b82213ee7fc5981f0c3a761cfa
Nathan Phan <nathanphan96@yahoo.com>
2023-03-20 23:57:50 -0700

    removed some unneeded code

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,18 +1,10 @@
-
-// Dashboard Routes
-// This is a set of routes that will be used to render the dashboard pages.
-// All of these routes will be protected by the withAuth middleware function.
-
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models/");
 const withAuth = require("../utils/auth");
 
-// TODO - create logic for the GET route for / that renders the dashboard homepage
-// It should display all of the posts created by the logged in user
+
 router.get("/", withAuth, async (req, res) => {
-  // TODO - retrieve all posts from the database for the logged in user
-  // render the dashboard template with the posts retrieved from the database
-  //default layout is set to main.handlebars, layout need to be changed to dashboard to use dashboard.handlebars
+
   try {
     const userPost = await Post.findAll({
       where: { userId: req.session.userId },
@@ -34,43 +26,18 @@ router.get("/", withAuth, async (req, res) => {
   } catch (error) {
     res.redirect("/login");
   }
-  // refer to admin-all-posts.handlebars write the code to display the posts
+
 });
 router.get("/create", withAuth, async (req, res) => {
   res.render("admin-create-post", { layout: "dashboard" });
   
   });
   
-// TODO - create logic for the GET route for /new that renders the new post page
-// It should display a form for creating a new post
+
 router.get("/new", withAuth, async (req, res) => {
   res.render("new-post", { layout: "dashboard" });
 });
-// TODO - create logic for the GET route for /edit/:id that renders the edit post page
-// It should display a form for editing an existing post
-router.put("/edit/:id", withAuth, async (req, res) => {
-  try {
-    const userPost = await Post.update({
-      where: {
-        id: req.params.id,
-      },
-      where: {
-        body: req.body.body,
-      },
-    });
 
-    if (userPost) {
-      const post = userPost.get({ plain: true });
-      console.log(post);
-      res.render("edit-post", {
-        layout: "dashboard",
-        post,
-      });
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.redirect("login");
-  }
-});
+
+
 module.exports = router;
